5cb2996a98f3c800e08e9754d42f7734760a9ffc
kaziErfan <erfanhtamim1@gmail.com>
2025-12-16 22:08:27 +0600

    printing 10 - 1 with while loop

--- loop.js
+++ loop.js
@@ -1,3 +1,5 @@
+//Printing 1-10 using for loop
+
 let elem = document.getElementById("for10");
 
 let result = "";
@@ -7,3 +9,20 @@ for (let i = 1; i <= 10; i++){
    result += i + '<br>';
 }
 elem.innerHTML = result; 
+
+// Printing 10 - 1 using while loop
+
+let whileElem = document.getElementById("while10");
+
+let whileResult = "";
+
+let j = 10;
+
+while(j >= 1){
+    console.log(j);
+    whileResult += j + '<br>';
+    j--;
+   
+}
+whileElem.innerHTML = whileResult;
+
